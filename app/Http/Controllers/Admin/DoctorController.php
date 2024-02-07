fb03d2b5e56e0f3214b43168d55385a83cc00a72
Trotteriniku <stefanozidda3@gmail.com>
2024-02-07 11:03:22 +0100

    controller namespace fixed

--- app/Http/Controllers/Admin/DoctorController.php
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Doctor;
 use App\Http\Requests\StoreDoctorRequest;
 use App\Http\Requests\UpdateDoctorRequest;
+use App\Http\Controllers\Controller;
 
 class DoctorController extends Controller
 {
