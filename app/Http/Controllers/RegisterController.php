572420d8f6abe88193e758cd5aa9e5fe5c124aa9
Trotteriniku <stefanozidda3@gmail.com>
2024-02-13 16:40:48 +0100

    Co-authored-by: SimoneNegrisoli <SimoneNegrisoli@users.noreply.github.com> Co-authored-by: Alberto Stizzoli <albertostizzoli@users.noreply.github.com> Co-authored-by: Francesco Carrara <fcarrara24@users.noreply.github.com>adding edit form

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Auth;
 
 class RegisterController extends Controller
 {
     public function index()
     {
-        return view('user.index');
+        return view('auth.register');
         //return view('auth.login');
     }
 
