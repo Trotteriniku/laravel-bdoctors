6da2f5fd296c5a09274ac267afff8ae658d4c6e2
Trotteriniku <stefanozidda3@gmail.com>
2024-02-14 12:25:59 +0100

    api controller fixed

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -11,14 +11,21 @@ class AccountController extends Controller
     // PRESI TUTTI I DATI DI ACCOUNT E CONVERTITI IN UN FILE JSON
     public function index()
     {
-        $accounts = Account::all();
-        return response()->json($accounts);
+        $accounts = Account::with(['specializations', 'sponsorships', 'ratings', 'user'])->get();
+        // return response()->json($accounts, );
+        return response()->json(
+            [
+                'success' => true,
+                'results' => $accounts
+            ]
+        );
+
     }
 
     // PRESI DATI ACCOUNT + ALTRE ENTITTA' E CONVERTITI IN FILE JSON
     public function show($id)
     {
-        $accounts = Account::where('id', $id)->with('specializations', 'sponsorships', 'ratings')->first();
+        $accounts = Account::where('id', $id)->with('specializations', 'sponsorships', 'ratings', 'user')->first();
         return response()->json($accounts);
     }
 }
