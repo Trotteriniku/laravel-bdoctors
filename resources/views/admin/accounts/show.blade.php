6ad5d16454816317f2ddb863d79b543068520b1b
Trotteriniku <stefanozidda3@gmail.com>
2024-02-16 16:07:17 +0100

    backoffice show layout

--- resources/views/admin/accounts/show.blade.php
+++ resources/views/admin/accounts/show.blade.php
@@ -1,128 +1,80 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container pt-5 w-100  ">
+    <div class="container mt-4 pt-5" style="background-color: #f6f9ff;">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
-            <div class="row justify-content-center pt-5">
-                <div class="col-6">
+                <div class="mb-4 d-flex justify-content-center align-items-center"
+                    style="background-color: #95D904; height: 300px;">
+                    {{-- <img src="{{ asset('path_to_your_image.jpg') }}" alt="Foto del medico"
+                        style="max-width: 100%; height: auto;"> --}}
 
-                    <div class="pb-2 card">
-                        <h1>Dottore da user</h1>
-                        <div>
-                            <span>nome</span>
-                            <span>{{$user->name}}</span>
-                        </div>
-                        <div>
-                            <span>cognome</span>
-                            <span>{{$user->surname}}</span>
-                        </div>
-                        <div>
-                            <span>email</span>
-                            <span>{{$user->email}}</span>
-                        </div>
-                    </div>
-
-                    <div class="pb-2 card">
-                        <h1>Dottore da account</h1>
-                        <div>
-                            <span>phone</span>
-                            <span>{{$account->phone}}</span>
-                        </div>
-                        <div>
-                            <span>indirizzo</span>
-                            <span>{{$account->address}}</span>
-                        </div>
-                    </div>
-
-                    <div class="pb-2 card">
-                        <h1>Dottore specializzazioni</h1>
-                        <div>
-                            <span>phone</span>
-                            <span>{{$account->phone}}</span>
-                        </div>
-                        <div>
-                            <span>indirizzo</span>
-                            <span>{{$account->address}}</span>
-                        </div>
-                    </div>
-
-                    <div class="pb-2 card">
-                        <h1>Dottore messaggi ricevuti</h1>
-                        @foreach ($messages as $message)
-                            <h3>messaggio</h3>
-                                <div>
-                                    <span class="text-uppercase">titolo</span>
-                                    <span>{{$message->title}}</span>
-                                </div>
-                                <div>
-                                    <span class="text-uppercase">nome</span>
-                                    <span>{{$message->name}}</span>
-                                </div>
-                                <div>
-                                    <span class="text-uppercase">useremail</span>
-                                    <span>{{$message->email}}</span>
-                                </div>
-                                <div>
-                                    <span class="text-uppercase">data</span>
-                                    <span>{{ substr($message->created_at, 0, 10)}}</span>
-                                </div>
-                                <div>
-                                    <span class="text-uppercase">contenuto</span>
-                                    <span>{{$message->content}}</span>
-                                </div>
-                        @endforeach
-
-                    </div>
-
-                    <div class="pb-2 card">
-                        <h1>Dottore recensioni ricevute</h1>
-                        @foreach ($reviews as $review)
-                            <h3>messaggio</h3>
-                                <div>
-                                    <span class="text-uppercase">nome</span>
-                                    <span>{{$review->name}}</span>
-                                </div>
-
-                                <div>
-                                    <span class="text-uppercase">titolo</span>
-                                    <span>{{$review->title}}</span>
-                                </div>
-
-                                <div>
-                                    <span class="text-uppercase">email</span>
-                                    <span>{{$review->email}}</span>
-                                </div>
-                                <div>
-                                    <span class="text-uppercase">data</span>
-                                    <span>{{ substr($review->created_at, 0, 10)}}</span>
-                                </div>
-                                <div>
-                                    <span class="text-uppercase">contenuto</span>
-                                    <span>{{$review->content}}</span>
-                                </div>
-
-                        @endforeach
-
-                    </div>
-
-
-                    <div class="pb-2 card">
-                        <h1>Dottore specializzazioni</h1>
-                        @foreach ($specializations as $specialization)
-                            <h3>specializzazione</h3>
-                                <div>
-                                    <span class="text-uppercase">nome</span>
-                                    <span>{{$specialization->name}}</span>
-                                </div>
-
-
-
-                        @endforeach
-
-                    </div>
+                    @if ($account->image !== '')
+                        <img class="img-fluid" src="{{ asset('images/Bdoctor.png') }}" alt="Foto del medico">
+                    @endif
 
                 </div>
-            </div>
 
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h1 class="card-title fw-semibold ">Informazioni Utente</h1>
+                        <div class="row " style="color: #0476D9">
+                            <div class="col-md-6">
+                                <p><strong>Nome:</strong> {{ $user->name }}</p>
+                                <p><strong>Cognome:</strong> {{ $user->surname }}</p>
+                                <p><strong>Email:</strong> {{ $user->email }}</p>
+                            </div>
+                            <div class="col-md-6">
+                                <p><strong>Telefono:</strong> {{ $account->phone }}</p>
+                                <p><strong>Indirizzo:</strong> {{ $account->address }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h1 class="card-title fw-semibold ">Messaggi Ricevuti</h1>
+                        @foreach ($messages as $message)
+                            <div class="border-bottom mb-3 pb-3">
+                                <h4 class="card-subtitle mb-2 text-muted" style="color: #0476D9">{{ $message->title }}</h4>
+                                <p><strong>Nome:</strong> {{ $message->name }}</p>
+                                <p><strong>Email:</strong> {{ $message->email }}</p>
+                                <p><strong>Data:</strong> {{ substr($message->created_at, 0, 10) }}</p>
+                                <p><strong>Contenuto:</strong> {{ $message->content }}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+
+                <div class="card mb-4 ">
+                    <div class="card-body">
+                        <h1 class="card-title fw-semibold ">Recensioni Ricevute</h1>
+                        @foreach ($reviews as $review)
+                            <div class="border-bottom mb-3 pb-3">
+                                <h4 class="card-subtitle mb-2 text-muted" style="color: #0476D9">{{ $review->title }}</h4>
+                                <p><strong>Nome:</strong> {{ $review->name }}</p>
+                                <p><strong>Email:</strong> {{ $review->email }}</p>
+                                <p><strong>Data:</strong> {{ substr($review->created_at, 0, 10) }}</p>
+                                <p><strong>Contenuto:</strong> {{ $review->content }}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+
+                <div class="card mb-4 ">
+                    <div class="card-body">
+                        <h1 class="card-title fw-semibold ">Specializzazioni</h1>
+                        @foreach ($specializations as $specialization)
+                            <div class="border-bottom mb-3 pb-3 " style="color: #0476D9">
+                                <p><strong>Nome:</strong> {{ $specialization->name }}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 @endsection
