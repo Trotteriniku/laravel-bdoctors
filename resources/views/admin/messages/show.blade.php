b78af64b85244fa737160c6908d47508c79210c3
albertostizzoli <albertostizzoli@yahoo.it>
2024-02-20 18:20:38 +0100

    adjustment layout table of messages and reviews

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -6,16 +6,16 @@
                 <div class="container">
                     <div class="row">
                         <h2 class=" w-100 card-title fw-semibold px-2">Messaggio</h2>
-                            <div class="w-100 pt-3">
-                                <div class="card">
-                                    <h1 class="text-center">{{ $message->name }}</h1>
-                                    <div class="card-body">
-                                        <h3 class="card-title">{{ $message->title }}</h3>
-                                        <p class="card-text">{{ $message->email }}</p>
-                                        <p class="card-text">{{ $message->content }}</p>
-                                    </div>
+                        <div class="w-100 pt-3">
+                            <div class="card">
+                                <h1 class="text-center">{{ $message->name }}</h1>
+                                <div class="card-body">
+                                    <h3 class="card-title">{{ $message->title }}</h3>
+                                    <p class="card-text">{{ $message->email }}</p>
+                                    <p class="card-text">{{ $message->content }}</p>
                                 </div>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
