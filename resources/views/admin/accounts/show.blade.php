29ab7969eae63b6e6d111313f3f1823844597898
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-02-20 11:17:02 +0100

    added image profile to show

--- resources/views/admin/accounts/show.blade.php
+++ resources/views/admin/accounts/show.blade.php
@@ -18,16 +18,22 @@
 
                 <div class="card mb-4">
                     <div class="card-body">
-                        <h1 class="card-title fw-semibold ">Informazioni Utente</h1>
+                        <div class="w-100  d-flex justify-content-center">
+                            <div class="box-img mt-3 ">
+                                <img src="{{ asset($account->image) }}" alt="hello">
+                            </div>
+                        </div>
+                        <h1 class="card-title fw-semibold border-bottom">Informazioni Utente</h1>
                         <div class="row " style="color: #0476D9">
                             <div class="col-md-6">
                                 <p><strong>Nome:</strong> {{ $user->name }}</p>
                                 <p><strong>Cognome:</strong> {{ $user->surname }}</p>
-                                <p><strong>Email:</strong> {{ $user->email }}</p>
+                                <p><strong>Indirizzo:</strong> {{ $account->address }}</p>
                             </div>
                             <div class="col-md-6">
+                                <p><strong>Email:</strong> {{ $user->email }}</p>
                                 <p><strong>Telefono:</strong> {{ $account->phone }}</p>
-                                <p><strong>Indirizzo:</strong> {{ $account->address }}</p>
+
                             </div>
                         </div>
                     </div>
@@ -35,7 +41,7 @@
 
                 <div class="card mb-4">
                     <div class="card-body">
-                        <h1 class="card-title fw-semibold ">Messaggi Ricevuti</h1>
+                        <h1 class="card-title fw-semibold border-bottom ">Messaggi Ricevuti</h1>
                         @foreach ($messages as $message)
                             <div class="border-bottom mb-3 pb-3">
                                 <h4 class="card-subtitle mb-2 text-muted" style="color: #0476D9">{{ $message->title }}</h4>
@@ -50,7 +56,7 @@
 
                 <div class="card mb-4 ">
                     <div class="card-body">
-                        <h1 class="card-title fw-semibold ">Recensioni Ricevute</h1>
+                        <h1 class="card-title fw-semibold border-bottom">Recensioni Ricevute</h1>
                         @foreach ($reviews as $review)
                             <div class="border-bottom mb-3 pb-3">
                                 <h4 class="card-subtitle mb-2 text-muted" style="color: #0476D9">{{ $review->title }}</h4>
@@ -65,7 +71,7 @@
 
                 <div class="card mb-4 ">
                     <div class="card-body">
-                        <h1 class="card-title fw-semibold ">Specializzazioni</h1>
+                        <h1 class="card-title fw-semibold border-bottom">Specializzazioni</h1>
                         @foreach ($specializations as $specialization)
                             <div class="border-bottom mb-3 pb-3 " style="color: #0476D9">
                                 <p><strong>Nome:</strong> {{ $specialization->name }}</p>
