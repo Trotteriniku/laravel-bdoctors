016f1f93fdd3540881b7cf38e0cf5cd816deac0b
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-02-22 17:45:46 +0100

    req. bugfix

--- app/Http/Controllers/Api/AccountFilter.php
+++ app/Http/Controllers/Api/AccountFilter.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Account;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class AccountFilter extends Controller
 {
