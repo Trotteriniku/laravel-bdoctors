967e032168ead9aaad38ab82ed3a19188a349a25
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-02-25 17:12:02 +0100

    db update

--- config/db.php
+++ config/db.php
@@ -216,6 +216,86 @@ return [
             'rating_id' => 1,
         ],
         [
+            'account_id' => 1,
+            'rating_id' => 4,
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 4,
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 5,
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 3,
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 2,
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 1
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 2
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 3
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 4
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 5
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 1
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 2
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 3
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 4
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 5
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 1
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 2
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 3
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 4
+        ],
+        [
+            'account_id' => 1,
+            'rating_id' => 5
+        ],
+        [
             'account_id' => 2,
             'rating_id' => 3,
         ],
@@ -319,6 +399,112 @@ return [
             'email' => 'mariorossi@example.com',
         ],
         [
+            'account_id' => 1,
+            'name' => 'Luca Bianchi',
+            'content' => 'È possibile avere un appuntamento per una consulenza?',
+            'title' => 'Consulenza',
+            'email' => 'lucabianchi@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Giulia Verdi',
+            'content' => 'Ho bisogno di un controllo di routine, quando posso venire?',
+            'title' => 'Controllo',
+            'email' => 'giuliaverdi@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Marco Neri',
+            'content' => 'Devo rinnovare la mia prescrizione, posso farlo online?',
+            'title' => 'Rinnovo',
+            'email' => 'marconeri@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Elena Ferrari',
+            'content' => 'Desidero cancellare il mio appuntamento di domani.',
+            'title' => 'Cancellazione ',
+            'email' => 'elenaferrari@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Marisa Bianchi',
+            'content' => 'Mi serve una visita specialistica al più presto, potete aiutarmi?',
+            'title' => 'Esami',
+            'email' => 'marisabianchi@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Francesca Martini',
+            'content' => 'Ho urgente bisogno di una consulenza dermatologica, avete disponibilità?',
+            'title' => 'Consulenza',
+            'email' => 'francescamartini@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Roberto Galli',
+            'content' => 'Vorrei sapere se effettuate visite ortopediche in clinica.',
+            'title' => 'TAC',
+            'email' => 'robertogalli@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Chiara Bruno',
+            'content' => 'Devo fare un controllo oculistico, quale è il primo appuntamento disponibile?',
+            'title' => 'Appuntamento',
+            'email' => 'chiarabruno@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Giovanni Ricci',
+            'content' => 'È possibile prenotare una visita per problemi di allergia?',
+            'title' => 'Visita',
+            'email' => 'giovanniricci@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Davide Bianchi',
+            'content' => 'C\'è la possibilità di effettuare un check-up completo entro la prossima settimana?',
+            'title' => 'Richiesta',
+            'email' => 'davidebianchi@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Laura Mancini',
+            'content' => 'Avrei bisogno di una consulenza nutrizionale, potete aiutarmi?',
+            'title' => 'Consulenza',
+            'email' => 'lauramancini@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Simone Ferrara',
+            'content' => 'Desidererei informazioni sulle procedure per gli esami del sangue.',
+            'title' => 'Informazioni',
+            'email' => 'simoneferrara@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Alessia Romano',
+            'content' => 'Posso prenotare una visita ginecologica urgente?',
+            'title' => 'Visita',
+            'email' => 'alessiaromano@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Matteo Costa',
+            'content' => 'Avrei necessità di una consulenza psicologica, avete disponibilità?',
+            'title' => 'Reclamo',
+            'email' => 'matteocosta@example.com',
+        ],
+        [
+            'account_id' => 1,
+            'name' => 'Elisa Rizzo',
+            'content' => 'Ho bisogno di un appuntamento per una valutazione ortodontica per mio figlio.',
+            'title' => 'Esame',
+            'email' => 'elisarizzo@example.com',
+        ],
+
+        [
             'account_id' => 4,
             'name' => 'Enzo Bianchi',
             'content' => 'Il mio problema mi si è ripresentato',
@@ -356,6 +542,98 @@ return [
             'content' => 'molto competente e premuroso nel suo lavoro. Tuttavia, spesso ho dovuto aspettare più del previsto per il mio turno',
             'account_id' => 3,
         ],
+        [
+            'name' => 'Marco Gentile',
+            'title' => 'Professionalità e Cortesia',
+            'email' => 'marco.gentile@example.com',
+            'content' => 'Estremamente professionale e cortese, ha risposto a tutte le mie domande con pazienza.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Anna Ricci',
+            'title' => 'Ottimo Servizio',
+            'email' => 'anna.ricci@example.com',
+            'content' => 'Servizio impeccabile e tempi di attesa ridotti al minimo. Consigliatissimo.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Luigi Bianchi',
+            'title' => 'Esperienza Positiva',
+            'email' => 'luigi.bianchi@example.com',
+            'content' => 'Una esperienza molto positiva, personale attento e disponibile.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Sofia Lorenzi',
+            'title' => 'Attesa Lunga ma Ne Vale la Pena',
+            'email' => 'sofia.lorenzi@example.com',
+            'content' => 'Nonostante un\'attesa più lunga del previsto, la qualità del servizio è stata eccellente.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Giovanni Rossi',
+            'title' => 'Eccellenza Medica',
+            'email' => 'giovanni.rossi@example.com',
+            'content' => 'Competenza e professionalità al top. Un vero punto di riferimento nel settore.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Laura Neri',
+            'title' => 'Consulenza Approfondita',
+            'email' => 'laura.neri@example.com',
+            'content' => 'Mi ha dedicato molto tempo per una consulenza approfondita, mostrando grande competenza.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Francesco Verdi',
+            'title' => 'Attento e Preciso',
+            'email' => 'francesco.verdi@example.com',
+            'content' => 'Medico molto attento e preciso nelle sue diagnosi, altamente raccomandato.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Chiara Mazzini',
+            'title' => 'Affidabile e Competente',
+            'email' => 'chiara.mazzini@example.com',
+            'content' => 'Affidabile e competente, sempre attenta alle esigenze del paziente.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Daniele Ferri',
+            'title' => 'Disponibilità e Supporto',
+            'email' => 'daniele.ferri@example.com',
+            'content' => 'Sempre disponibile e di supporto, fa sentire il paziente a proprio agio.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Elena Grimaldi',
+            'title' => 'Una Garanzia',
+            'email' => 'elena.grimaldi@example.com',
+            'content' => 'Professionalità assicurata. Una garanzia per chi cerca cure di alto livello.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Simone Conti',
+            'title' => 'Rapporto Qualità-Prezzo',
+            'email' => 'simone.conti@example.com',
+            'content' => 'Ottimo rapporto qualità-prezzo. Servizi medici di alta qualità a costi accessibili.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Valentina Amato',
+            'title' => 'Cura e Attenzione',
+            'email' => 'valentina.amato@example.com',
+            'content' => 'Cura e attenzione per il paziente sono le sue priorità. Altamente soddisfatta.',
+            'account_id' => 1,
+        ],
+        [
+            'name' => 'Michele Lombardi',
+            'title' => 'Risultati Eccellenti',
+            'email' => 'michele.lombardi@example.com',
+            'content' => 'Risultati eccellenti grazie a metodologie all\'avanguardia',
+            'account_id' => 1,
+        ],
+
         ['name' => 'Ludovica Canova', 'title' => 'Competenza vs Disponibilità', 'email' => 'martina18@pasqua-travaglio.it', 'content' => 'estremamente gentile e paziente, ma talvolta la sua disponibilità per appuntamenti last-minute è limitata.', 'account_id' => 7],
         ['name' => 'Sig.ra Emma Donatoni', 'title' => 'Professione e Cortesia', 'email' => 'stoppaniornella@virgilio.it', 'content' => 'molto professionale e preciso nelle sue diagnosi,  il suo staff è cordiale e accogliente.', 'account_id' => 2],
         ['name' => 'Piermaria Giannuzzi', 'title' => 'Un Mix di Esperienze', 'email' => 'burcardodonatello@farina-russo.com', 'content' => 'Ho ricevuto un trattamento eccezionale ma il parcheggio presso il suo studio è sempre un incubo."', 'account_id' => 6],
